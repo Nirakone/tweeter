78d43d9d1a655791ab515523cfb4fe922af657fd
Nirakone Phromkharanourak <119075500+Nirakone@users.noreply.github.com>
2023-07-10 20:46:53 -0400

    Add files via upload

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,19 @@
+//character counter
+$(document).ready(function() {
+  let tweetLimit = 140;
+
+  $("#tweet-text").keyup(function() {
+
+    let charactersRemaining = tweetLimit - $(this).val().length;
+
+    $(".counter").text(charactersRemaining);
+
+    if (charactersRemaining < 0) {
+      $(".counter").addClass("counter-overlimit");
+    } else {
+      $(".counter").removeClass("counter-overlimit");
+    }
+
+  });
+
+});
